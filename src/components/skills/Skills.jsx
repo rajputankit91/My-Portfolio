1b7da278ce70c3981268ac688caac348e6cc2d27
aharankit <jet.ankit@gmail.com>
2024-03-10 22:21:39 +0530

    portfolio

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -55,11 +55,8 @@ const Skills = () =>{
                 highly-motivated, result oriented, self-driven, hard-working,
                 fast learner and constantly seeking to improve my skills and am
                 fully aware of the latest Front-end Development Tools. I have a
-                firm knowledge of JavaScript and React Js, Parcel js, Node js,
-                Redux, HTML5, CSS3, Git, SASS, Tailwind CSS, Bootstrap, REST
-                API, JSON, Axios, Adobe Photoshop and possess a good knowledge
-                of computer software packages (frameworks and tools) that are
-                used in today’s technology.
+                firm knowledge of JavaScript and React Js, Redux, HTML5, CSS3, Git, REST
+                API, JSON and Axios.
               </p>
             </div>
 
