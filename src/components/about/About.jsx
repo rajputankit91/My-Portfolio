dbfc455b720d755f844e65816f3de7151a2a1ded
Ankit Kumar <ankitrajput10791@gmail.com>
2024-08-24 21:28:37 +0530

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,28 +5,40 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa6";
 import Aos from "aos";
 import "aos/dist/aos.css";
-const About = () =>{
+import { Typewriter } from "react-simple-typewriter";
+const About = () => {
 
-  useEffect(() =>{
-    Aos.init({duration:2000});
-  },[])
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, [])
 
   return (
     <section className="intro" id="intro">
       <div className="i-left" data-aos="fade-down-right" data-aos-easing="ease-out-cubic" data-aos-duration="1500">
-        
+
         <div className="i-name">
           <span>Hey there, </span>
           <span>Ankit here 👋</span>
-          <span>and I'm a Frontend Developer</span>
+          {/* <span>and I'm a Frontend Developer</span> */}
+          <span>and I'm a 
+            <Typewriter
+              words={[" Frontend Developer"]}
+              loop={500}
+              cursor
+              cursorStyle='_'
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </span>
         </div>
 
         <a className="resume" href="resume.pdf" download="resume.pdf">
           Download Resume
         </a>
-        
+
         <div className="i-icons">
-        
+
           <a className="resume" href="https://github.com/rajputankit91" target="_blank">
             <FaGithub className="gitHub Icon" />
           </a>
@@ -34,9 +46,9 @@ const About = () =>{
           <a className="resume" href="https://www.linkedin.com/in/ankit-kumar-5180b7283/" target="_blank">
             <FaLinkedinIn className="linkedIn Icon" />
           </a>
-        
+
         </div>
-      
+
       </div>
 
       <div className="i-right" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1500">
